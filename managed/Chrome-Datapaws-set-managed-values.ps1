b4dae6a173339f4129b10344caf5a95ee5bc4f2a
G.caglio <G.caglio@Cantylab.net>
2026-01-26 06:44:14 +0100

    added sample chrome managed storage for windows

---
+++ managed/Chrome-Datapaws-set-managed-values.ps1
@@ -0,0 +1,43 @@
+# ===== CONFIGURAZIONE =====
+$chromeExtensionId = "bconhdjdcokejhjjgpinbmjofmgkpeki"
+
+# Parametri da passare all'estensione
+$managedConfig = @{}
+
+#https://api.datadoghq.com
+#https://api.datadoghq.eu
+$managedConfig.Add("dd_url", "https://api.datadoghq.com")
+
+# your API key
+$managedConfig.Add("dd_api", "xxxxxxxxxx")
+
+# hostname sent to datadog for all the metrics
+$managedConfig.Add("dd_hostname", "datapaws.unique.host.local")
+
+# tag list, separated by ;
+$managedConfig.Add("dd_custom_tags", "site=CHANGEME;building=CHANGEME;regione=CHANGEME")
+
+# ===== CHROME =====
+Write-Host "Configuring Chrome..." 
+
+# Debug
+Write-Host "Type of managedConfig: $($managedConfig.GetType().Name)" 
+Write-Host "Keys: $($managedConfig.Count)" 
+
+# Configure parameters in Managed Storage
+$chromeManagedPath = "HKLM:\SOFTWARE\Policies\Google\Chrome\3rdparty\extensions\$chromeExtensionId\policy"
+
+if (!(Test-Path $chromeManagedPath)) {
+    New-Item -Path $chromeManagedPath -Force | Out-Null
+}
+
+# Write reg key for each params
+foreach ($key in $managedConfig.Keys) {
+    Write-Host "  Scrivo: $key = $($managedConfig[$key])" -ForegroundColor Gray
+    New-ItemProperty -Path $chromeManagedPath -Name $key -Value $managedConfig[$key] -PropertyType String -Force | Out-Null
+}
+
+Write-Host "Chrome configured!"
+
+Write-Host "Written values:"
+Get-ItemProperty -Path $chromeManagedPath | Format-List dd_*
