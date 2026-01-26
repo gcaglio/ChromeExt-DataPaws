8d22bc907a10cd66d8e15ff1736783d2f3f3138c
G.caglio <G.caglio@Cantylab.net>
2026-01-26 06:52:25 +0100

    Update Chrome extension ID in PowerShell script

--- managed/Chrome-Datapaws-set-managed-values.ps1
+++ managed/Chrome-Datapaws-set-managed-values.ps1
@@ -2,7 +2,7 @@
 
 # CUSTOMIZE WITH CURRENT EXTENSION ID FROM WEB STORE.
 # you can find the extension ID in the URL when you select Datapaws extension from the Web Store!
-$chromeExtensionId = "bconhdjdcokejhjjgpinbmjofmgkpeki"
+$chromeExtensionId = "hihojofmgncccdhbhglbchhigdgbhdhp"
 
 # Parametri da passare all'estensione
 $managedConfig = @{}
